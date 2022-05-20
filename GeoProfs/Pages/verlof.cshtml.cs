f25f7aa2fbe6d2d7d31c6f40a19dec9ed43d5808
thimovee <thimove@gmail.com>
2022-05-20 14:38:41 +0200

    Add project files.

---
+++ GeoProfs/Pages/verlof.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeoProfs.Pages
+{
+    public class Verlof : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
